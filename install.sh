25ffeb9ab902eb95df905c1127f95e9f2f24d824
Gabriel Rubens <grsabreu@gmail.com>
2016-12-05 22:12:36 -0200

    Put zsh setup in the very bottom and minor updates

--- install.sh
+++ install.sh
@@ -1,36 +1,57 @@
 #!/bin/sh
-sudo apt-get install build-essentials
-sudo apt-get install git-all
-sudo apt-get install vim
-sudo apt-get install tmux
-sudo apt-get install zsh
-sudo apt-get install curl
-sudo apt-get install tree
-sudo apt-get install vlc
-sudo apt-get install google-chrome-stable
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install build-essential -y
+sudo apt-get install git-all -y
+sudo apt-get install vim -y
+sudo apt-get install tmux -y
+sudo apt-get install zsh -y
+sudo apt-get install curl -y
+sudo apt-get install tree -y
+sudo apt-get install vlc -y
+sudo apt-get install chromium -y
+sudo apt-get install unity-tweak-tool -y
+
+# Install Java
+sudo apt-get install default-jre -y
+sudo apt-get install default-jdk -y
 
 # Install Paper
-sudo add-apt-repository ppa:snwh/pulp
+sudo add-apt-repository ppa:snwh/pulp -y
 # update repository info
 sudo apt-get update
 # install icon theme
-sudo apt-get install paper-icon-theme
+sudo apt-get install paper-icon-theme -y
 # install gtk theme
-sudo apt-get install paper-gtk-theme
+sudo apt-get install paper-gtk-theme -y
 # install cursor theme
-sudo apt-get install paper-cursor-theme
+sudo apt-get install paper-cursor-theme -y
+
+# Install Source Code Pro
+# Thanks to https://github.com/adobe-fonts/source-code-pro/issues/17#issuecomment-102803992
+FONT_HOME=~/.local/share/fonts
+
+echo "installing fonts at $PWD to $FONT_HOME"
+mkdir -p "$FONT_HOME/adobe-fonts/source-code-pro"
+# find "$FONT_HOME" -iname '*.ttf' -exec echo '{}' \;
+
+(git clone \
+   --branch release \
+   --depth 1 \
+   'https://github.com/adobe-fonts/source-code-pro.git' \
+   "$FONT_HOME/adobe-fonts/source-code-pro" && \
+fc-cache -f -v "$FONT_HOME/adobe-fonts/source-code-pro")
 
 # Setup git
 git config --global user.email "grsabreu@gmail.com"
 git config --global user.name "Gabriel Rubens"
 
-# Setup zsh
-chsh -s /bin/zsh user
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # Setup Vim
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+echo "Copy my_config.vim to ~/.vim_runtime/my_config.vim"
 
 # Install NVM
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
@@ -45,12 +66,14 @@ curl https://install.meteor.com/ | sh
 # Setup React Native
 yarn global add react-native-cli
 
-# Install Java
-sudo apt-get install default-jre
-sudo apt-get install default-jdk
-
 # Setup Android SDK
 ANDROID_SDK_URL=https://dl.google.com/dl/android/studio/ide-zips/2.2.2.0/android-studio-ide-145.3360264-linux.zip
 curl $ANDROID_SDK_URL -o ~/android_sdk.zip
 unzip ~/android_sdk.zip
-mv ~/android_sdk ~/Android
+mv ~/android-studio ~/Android
+
+# Setup zsh
+chsh -s /bin/zsh user
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "Logout and login again to get zsh as default term"
